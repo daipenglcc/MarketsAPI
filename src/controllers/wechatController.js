feda7697a5fe70dc3519516482181f527894a8db
daipeng <youhuabujianye@gmail.com>
2024-12-27 17:12:23 +0800

    feat: 优化公众号模板

--- src/controllers/wechatController.js
+++ src/controllers/wechatController.js
@@ -1,9 +1,10 @@
 const wechatService = require('../services/wechatService')
+const utils = require('../utils/utils')
 const fs = require('fs')
 const path = require('path')
 const dayjs = require('dayjs')
 const weekOfYear = require('dayjs/plugin/weekOfYear')
-
+const chineseLunar = require('chinese-lunar')
 dayjs.extend(weekOfYear)
 
 async function fetchToken(ctx) {
@@ -28,25 +29,36 @@ async function fetchMediaList(ctx) {
 }
 
 async function addDraft(ctx) {
-	// const { articles } = ctx.request.body
 	const contentPath = path.join(__dirname, 'content.html')
 	let content = fs.readFileSync(contentPath, 'utf-8')
+	const lunar = chineseLunar.solarToLunar(new Date())
 
-	// 使用 dayjs 获取当前日期和周数
-	const now = dayjs()
-	const currentDate = now.format('YYYY年MM月DD日') // 当前日期
-	const weekNumber = now.week() // 当前周数
+	// 当前日期
+	const currentDate = dayjs().format('YYYY年MM月DD日')
+	// 当前周数
+	const weekNumber = dayjs().week()
+	// 当前是周几
+	const dayOfWeek = dayjs().day() // 返回数字 0（周日）到 6（周六）
+	const daysInChinese = ['周日', '周一', '周二', '周三', '周四', '周五', '周六']
+	// 获取生肖
+	const zodiacSigns = ['鼠', '牛', '虎', '兔', '龙', '蛇', '马', '羊', '猴', '鸡', '狗', '猪']
+	const zodiac = zodiacSigns[(lunar.year - 4) % 12]
+	// 获取农历日期
+	const formattedLunarDate = utils.formatLunar(lunar)
+	// 格式组装
+	const formatted = `农历${lunar.year}【${zodiac}】年${formattedLunarDate}`
 
-	console.log('currentDate', currentDate)
-	console.log('weekNumber', weekNumber)
-	// content = content.replace('?周末?', currentDate)
+	content = content.replace(/\?当前日期\?/g, currentDate)
+	content = content.replace(/\?当前周\?/g, weekNumber + '周')
+	content = content.replace(/\?当前周几\?/g, daysInChinese[dayOfWeek])
+	content = content.replace(/\?当前农历\?/g, formatted)
 
 	try {
 		const result = await wechatService.addDraft([
 			{
 				title: '济南大集，周四提醒！',
 				author: '集集有你小助手',
-				digest: '希望是灵魂的心 Hope is the heartbeat of the soul',
+				// digest: '希望是灵魂的心 Hope is the heartbeat of the soul',
 				content,
 				thumb_media_id: 'xiWEbz3LGTAp4Uf9H93AyofOxjOB18SjwHX1WiPidoHG7dnTwQL4Y2P5lzmCvKVT'
 			}
